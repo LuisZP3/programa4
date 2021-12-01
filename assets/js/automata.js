e8f6541fd2389da0b79803120c3dac5cd4889dab
shamakunjod <juanariza@uninorte.edu.co>
2021-11-30 23:54:30 -0500

    version2
    ahora ilumina las letras

--- assets/js/automata.js
+++ assets/js/automata.js
@@ -8,6 +8,16 @@ const sleep = (time) => {
 }
 
 function obtenerSimbolos(){
+	var cell = document.getElementById("cambiandocolor");
+	if ( cell.hasChildNodes() )
+	{
+	while ( cell.childNodes.length >= 1 )
+	{
+	cell.removeChild( cell.firstChild );
+	}
+	}
+	document.getElementById("validar").disabled = true;
+
 	let simbolos=[];
 	var slider = document.querySelector("#myRange");
 	milisegundos = slider.value*5;
@@ -15,12 +25,36 @@ function obtenerSimbolos(){
 	let inputText = document.querySelector("#palabra").value;
 	for (var i = 0; i < inputText.length; i++)
 		simbolos[i]=inputText[i];
-	document.querySelector("#inputCinta").value = simbolos;
+	//document.querySelector("#inputCinta").value = simbolos;
 	reemplazarLetras(simbolos);
 }
 
 async function reemplazarLetras(simbolos){
 	for (var i = 0; i < simbolos.length; i++) {
+		let x=document.createElement("div");
+		x.id=i;
+		x.className="negro enlinea"
+		x.textContent=simbolos[i];
+		document.querySelector("#cambiandocolor").appendChild(x);
+	}
+	for (var i = 0; i < simbolos.length; i++) {
+		let x=document.getElementById(i);
+		x.className="rojo enlinea"
+		await sleep(milisegundos);
+		if(x.textContent=="b")
+			x.textContent="a";
+		else if(x.textContent==" ")
+			x.textContent=" ";
+		else if(x.textContent=="a")
+			x.textContent="a";
+		else{
+			aceptacion=false;
+			break;
+		}
+		x.className="negro enlinea"
+	}
+	irAlEstadoDeAceptacion(simbolos);
+/* 	for (var i = 0; i < simbolos.length; i++) {
 		await sleep(milisegundos);
 		if(simbolos[i]=="b")
 			simbolos[i]="a";
@@ -32,20 +66,27 @@ async function reemplazarLetras(simbolos){
 			aceptacion=false;
 			break;
 		}
+		
 		document.querySelector("#inputCinta").value = simbolos;
 	}
-	irAlEstadoDeAceptacion(simbolos);
+	irAlEstadoDeAceptacion(simbolos); */
 }
 
-function irAlEstadoDeAceptacion(simbolos){
+async function irAlEstadoDeAceptacion(simbolos){
 	for (var i = simbolos.length-1; i >= 0; i--) {
-		if(simbolos[i]=="a" || simbolos[i]==" "){
-			//Visual
+		let y=document.getElementById(i);
+		y.className="rojo enlinea";
+		await sleep(milisegundos);		
+		if(y.textContent=="a" || y.textContent==" "){
+			//
 		}else{
 			aceptacion=false;
 			break;
 		}
-	}
+		if(i>0){
+		y.className="negro enlinea";
+		}
+	}	
 	validarAceptacion(aceptacion);
 }
 
@@ -57,4 +98,5 @@ function validarAceptacion(aceptada){
 		modal.style.display = "block";
 		document.querySelector("#parrafoValidacion").innerText = "Palabra no válida";
 	}
+	document.getElementById("validar").disabled = false;
 }
